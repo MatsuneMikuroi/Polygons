05242d031728ffff9b5cc85d4b80711d26a997e0
MatsuneMikuroi <romain.degroote@gmail.com>
2025-01-10 03:19:01 +0100

    First py files

---
+++ Python/polygons.py
@@ -0,0 +1,11 @@
+class polygon:
+    def __init__(self, nb_side:int):
+        self.nb_side:int = nb_side;
+        self.per = 0;
+        self.a = 0;
+
+    def perimeter(self):
+        pass;
+
+    def area(self):
+        pass;
---
+++ Python/triangle.py
@@ -0,0 +1,5 @@
+import polygons as pg;
+
+class triangle:
+    def __init__(self) -> None:
+        pass
